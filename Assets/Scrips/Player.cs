153382530aa0b2e217f3df4b32f6092f25682488
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2024-03-29 23:48:17 +0900

    캐릭터 이동

--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("플레이어 이동")]
+    [SerializeField] float moveSpeed = 3f;
+    Vector2 movePos;
+    Rigidbody2D rigid;
+    Animator anim;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+
+        movePos.x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x + movePos.x, transform.position.y);
+        anim.SetBool("walk", movePos.x != 0);
+        if(movePos.x<0)
+        {
+            gameObject.transform.localScale = new Vector3(-1.5f, 1.5f, 1);
+        }
     }
 }
